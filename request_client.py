f7453a5218462658a4d1f3b9680106bb46233a94
Jensen Dempsey <jdemp@jensendempsey.com>
2015-05-04 11:49:57 -0500

    added basic logging to modules

--- request_client.py
+++ request_client.py
@@ -1,14 +1,16 @@
 __author__ = 'jdemp'
 from socket import*
-serverName ='localhost'
+serverName = 'localhost'
 serverPort = 8002
-clientSocket=socket(AF_INET,SOCK_DGRAM)
+clientSocket = socket(AF_INET,SOCK_DGRAM)
+sensor_name = ''
 
 message = input('Input lowercase sentence: ')
+message = sensor_name + ':' + message
 
-clientSocket.sendto(message.encode(),(serverName,serverPort))
+clientSocket.sendto(message.encode(), (serverName, serverPort))
 
-modifiedMessage,serverAddress=  clientSocket.recvfrom(2048)
+modifiedMessage,serverAddress = clientSocket.recvfrom(2048)
 
 print(modifiedMessage.decode())
 clientSocket.close()
--- response_server.py
+++ response_server.py
@@ -1,13 +1,24 @@
 __author__ = 'jdemp'
 from socket import*
+import time
+import logging
 serverPort = 8002
-serverSocket=socket(AF_INET, SOCK_DGRAM)
-
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+sensor_name = ''
 serverSocket.bind(('', serverPort))
 
 print('The server is ready to receive')
 
+logging.basicConfig(filename='pi.log', format='%(asctime)s %(message)s', datefmt='%d-%m-%Y %H:%M',
+                    level=logging.INFO)
+logging.info('Started')
+
 while 1:
     message, clientAddress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.upper()
-    serverSocket.sendto(modifiedMessage, clientAddress)
+    message_decode = message.decode()
+    client = message_decode.split(':')[0]
+    request = message_decode.split(':')[1]
+    logging.info('REQUEST from ' + client + ' | ' + request)
+    modifiedMessage = sensor_name + ':' + request.upper() + ' ' + time.strftime('%d-%m-%Y %H:%M')
+    logging.info('RESPONSE to ' + client + ' | ' + modifiedMessage)
+    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
